abc2a1123f2fb9ebbbd945e2e3adc9c0506fa7c5
Tanner Helton <tannerwh@gmail.com>
2017-09-26 12:25:08 -0500

    Test

---
+++ NightDay/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#orb').click(function () {
+        if ($(this).hasClass('sun')) {
+            $(this).removeClass('sun').addClass('moon');
+        } else {
+            $(this).removeClass('moon').addClass('sun');
+        }
+
+        if ($('#sky').hasClass('day')) {
+            $('#sky').removeClass('day').addClass('night');
+        } else {
+            $('#sky').removeClass('night').addClass('day');
+        }
+
+        if ($('[id^=moonspot').hasClass('visible')) {
+            $('[id^=moonspot').removeClass('visible');
+        } else {
+            $('[id^=moonspot').addClass('visible');
+        }
+
+        if ($('#planet1').hasClass('light')) {
+            $('#planet1').removeClass('light').addClass('dark');
+        } else {
+            $('#planet1').removeClass('dark').addClass('light');
+        }
+
+        if ($('#planet2').hasClass('light')) {
+            $('#planet2').removeClass('light').addClass('dark');
+        } else {
+            $('#planet2').removeClass('dark').addClass('light');
+        }
+    });
+});
