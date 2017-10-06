0c3260a33fd796b0634d9f5fb2fbd487d1db85aa
Tanner Helton <tannerwh@gmail.com>
2017-10-06 10:42:35 -0500

    finish node

--- NodeDemo/Helton.js
+++ NodeDemo/Helton.js
@@ -10,18 +10,19 @@ var a = 0;
 var b = 0;
 var c = 0;
 
+var radius = 0;
 var area = length * width;
 
 if (width > length) {
-    a = (2 * .25 * 3.14159 * length * length) / area * 100;
+    a = (((3.14159 * length * length) / 4) * 2) / area * 100;
     b = (Math.pow(length / 2, 2) * 3.14159) / area * 100;
     c = ((Math.pow(width / 4, 2) * 3.14159) * 2) / area * 100;
 } else if (length > width) {
-    a = (2 * .25 * 3.14159 * width * width) / area * 100;
+    a = (((3.14159 * width * width) / 4) * 2) / area * 100;
     b = (Math.pow(width / 2, 2) * 3.14159) / area * 100;
     c = ((Math.pow(length / 4, 2) * 3.14159) * 2) / area * 100;
-} else {
-    a = (2 * .25 * 3.14159 * width * width) / area / 2 * 100;
+} else if (width == length) {
+    a = (((3.14159 * length * length) / 4) * 2) / area / 2 * 100;
     b = (Math.pow(width / 2, 2) * 3.14159) / area * 100;
     c = ((Math.pow(length / 4, 2) * 3.14159) * 2) / area * 100;
 }
@@ -32,10 +33,20 @@ if (a > b && a > c) {
     }
 } else if (b > a && b > c) {
     if (b != a || b != c) {
-        console.log("A best");
+        console.log("B best");
+    }
+} else if (c > a && c > b) {
+    if (c != a || c != b) {
+        console.log("C best");
     }
 } else {
-    console.log("C best");
+    if (a == b) {
+        console.log("A best");
+    } else if (b == c) {
+        console.log("B best");
+    } else if (a == c) {
+        console.log("A best");
+    }
 }
 
 console.log('A: ' + a);
