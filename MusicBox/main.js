5869789115b8591dc49ea2c381acaf35b6973023
Tanner Helton <tannerwh@gmail.com>
2017-11-13 14:19:17 -0600

    Test

--- MusicBox/main.js
+++ MusicBox/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+    //test
     var cNote = document.getElementById('cAudio');
     $('#c').mousedown(function () {
         cNote.currentTime = 0;
@@ -8,6 +8,18 @@ $(document).ready(function () {
     $('#c').mouseover(function () {
         cNote.currentTime = 0;
         cNote.play();
+    });
+    $(document).keypress(function (event) {
+        if (event.key == 'q') {
+            cNote.currentTime = 0;
+            cNote.play();
+            $('#c').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'q') {
+            $('#c').toggleClass('active');
+        }
     });
 
     var dNote = document.getElementById('dAudio');
@@ -19,6 +31,18 @@ $(document).ready(function () {
         dNote.currentTime = 0;
         dNote.play();
     });
+    $(document).keypress(function (event) {
+        if (event.key == 'w') {
+            dNote.currentTime = 0;
+            dNote.play();
+            $('#d').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'w') {
+            $('#d').toggleClass('active');
+        }
+    });
 
     var eNote = document.getElementById('eAudio');
     $('#e').mousedown(function () {
@@ -28,6 +52,18 @@ $(document).ready(function () {
     $('#e').mouseover(function () {
         eNote.currentTime = 0;
         eNote.play();
+    });
+    $(document).keypress(function (event) {
+        if (event.key == 'e') {
+            eNote.currentTime = 0;
+            eNote.play();
+            $('#e').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'e') {
+            $('#e').toggleClass('active');
+        }
     });
 
     var fNote = document.getElementById('fAudio');
@@ -39,6 +75,18 @@ $(document).ready(function () {
         fNote.currentTime = 0;
         fNote.play();
     });
+    $(document).keypress(function (event) {
+        if (event.key == 'r') {
+            fNote.currentTime = 0;
+            fNote.play();
+            $('#f').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'r') {
+            $('#f').toggleClass('active');
+        }
+    });
 
     var gNote = document.getElementById('gAudio');
     $('#g').mousedown(function () {
@@ -48,6 +96,18 @@ $(document).ready(function () {
     $('#g').mouseover(function () {
         gNote.currentTime = 0;
         gNote.play();
+    });
+    $(document).keypress(function (event) {
+        if (event.key == 't') {
+            gNote.currentTime = 0;
+            gNote.play();
+            $('#g').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 't') {
+            $('#g').toggleClass('active');
+        }
     });
 
     var bNote = document.getElementById('bAudio');
@@ -59,6 +119,18 @@ $(document).ready(function () {
         bNote.currentTime = 0;
         bNote.play();
     });
+    $(document).keypress(function (event) {
+        if (event.key == 'u') {
+            bNote.currentTime = 0;
+            bNote.play();
+            $('#b').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'u') {
+            $('#b').toggleClass('active');
+        }
+    });
 
     var aNote = document.getElementById('aAudio');
     $('#a').mousedown(function () {
@@ -68,6 +140,18 @@ $(document).ready(function () {
     $('#a').mouseover(function () {
         aNote.currentTime = 0;
         aNote.play();
+    });
+    $(document).keypress(function (event) {
+        if (event.key == 'y') {
+            aNote.currentTime = 0;
+            aNote.play();
+            $('#a').toggleClass('active');
+        }
+    });
+    $(document).keyup(function (event) {
+        if (event.key == 'y') {
+            $('#a').toggleClass('active');
+        }
     });
 
 });
