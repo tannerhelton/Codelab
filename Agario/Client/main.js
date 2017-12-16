6c9417c7432c9c1afbad6448b37103b18a778a18
Tanner Helton <tannerwh@gmail.com>
2017-12-16 16:57:23 -0600

    Finish nodechat

--- Agario/Client/main.js
+++ Agario/Client/main.js
@@ -1,43 +1,53 @@
 var mainUser = '';
 var users = [];
-var socketServer = 'http://localhost:3000/';
+//Sets the ip of the server
+var socketServer = 'http://10.10.101.132:3000/';
 var socket = io.connect(socketServer);
 var user;
 
 
-var x = 0;
-var y = 0;
-
 function setup() {
-
+    //when connected to the server this will check for errors
     socket.on('connect', function () {
         $('#error').hide();
         $('#error').html('');
         $('#noerror').show();
     });
 
+    //When there is an error it will display
     socket.io.on('connect_error', function (err) {
         $('#error').html(err + '<br>Chat server offline');
         $('#error').show();
         $('#noerror').hide();
     });
 
+    //Used for when the user is first created. It copies existing users to this machnie's array
+    socket.on('start', function (data) {
+        users = data;
+    });
+
+    //WHen a new user is connected it display's it in the chat
     socket.on('welcome', function (data) {
         printOut(data.text);
     });
 
+    //When a new user is made this is called and it adds the data to the array
     socket.on('otherUserConnect', function (data) {
         printOut(data + ' connected');
+        users.push(data);
     });
 
+    //When another user leaves this will remove him from the array
     socket.on('otherUserDisconnect', function (data) {
-        printOut(data);
+        printOut(data.name);
+        for (var i = users.length - 1; i >= 0; --i) {
+            if (users[i].id == data.id) {
+                users.splice(i, 1);
+            }
+        }
     });
 
-    socket.on('connectUser', function (data) {
-        mainUser = data.text;
-    });
-
+    //When the save button is clicked it will add to the array and send it to the server
     $('#user-save').click(function () {
         var username = $('#user-name');
         var txt = username.val().trim();
@@ -49,61 +59,56 @@ function setup() {
             $('#message').prop('disabled', false);
             $('#restart').prop('disabled', false);
             user = name;
-            socket.emit('user', user);
-            users.push({
-                x: 0,
-                y: 0,
+            mainUser = user;
+            socket.emit('user', {
                 name: user
             });
         }
     });
 
-
+    //Simple button to restart the page, not very important
     $('#restart').click(function () {
         location.reload();
     });
 
+    //Made it easier to log things to the message area above the canvas
     function printOut(msg) {
         $('<div/>').text(msg).appendTo('#log');
     }
 
+    //Called by the server to update the data in the users array with the other users data
     socket.on('update', function (stuff) {
-        var p = 0;
         for (var i = 0; i < users.length; i++) {
-            if (users[i].name == stuff.name) {
+            if (users[i].id == stuff.id) {
                 users[i].x = stuff.x;
+                users[i].name = stuff.name;
                 users[i].y = stuff.y;
-            } else {
-                if (p >= users.length) {
-                    users.push({
-                        x: stuff.x,
-                        y: stuff.y,
-                        name: stuff.name
-                    });
-                } else {
-                    p += 1;
-                }
             }
         }
     });
-    createCanvas($(window).width() - 50, $(window).height() / 1.5);
+
+    //Creates the canvas for the sketches
+    createCanvas(800, 600);
 }
 
+//Called to draw what is displayed in the canvas
 function draw() {
     background(200);
-    x = mouseX;
-    y = mouseY;
+    //Everytime draw is run it sends an updated x and y to the server so that the other computer's can update their location for this machine's user
     socket.emit('data', {
         x: mouseX,
-        y: mouseY,
-        name: mainUser
+        y: mouseY
     });
     noStroke();
     fill('black');
     textSize(32);
+    //Shows a circle at the x and y
+    ellipse(mouseX, mouseY, 15, 15);
+
+    //Displays the other users' ellipses if there are any
     if (users.length >= 1) {
         for (var i = 0; i < users.length; i++) {
-            text(users[i].name, users[i].x, users[i].y - 25);
+            //text(users[i].name, users[i].x, users[i].y - 25);
             ellipse(users[i].x, users[i].y, 15, 15);
         }
     }
--- Agario/Server/app.js
+++ Agario/Server/app.js
@@ -1,72 +1,78 @@
+//This is used for creating a server with express
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+//Sets the root's directory name in the server
 var webroot = __dirname + '/../client/';
 
+//Tells the server to use the root
 app.use('/', express.static(webroot));
 
+//Creates a server on port 3000
 var server = http.listen(3000, function () {
     console.log('hosting from ' + webroot);
     console.log('server listening on http://localhost:3000/');
 });
 
-var users = [];
-var usr = [];
-
+//Sets up socket.io so that we can actually do things with the server
 var io = require('socket.io').listen(server);
 
-var currentConnections = {};
+//Create an array to later store the conencted users and their data
+var users = [];
+
 io.sockets.on('connection', function (socket) {
-    currentConnections[socket.id] = {
-        socket: socket
-    };
-    socket.on('data', function (someData) {
-        currentConnections[socket.id].x = someData.x;
-        currentConnections[socket.id].y = someData.y;
-        currentConnections[socket.id].name = someData.name;
-        socket.emit('update', {
-            x: currentConnections[socket.id].x,
-            y: currentConnections[socket.id].y,
-            name: currentConnections[socket.id].name
-        });
-    });
-    var clientIp = socket.request.connection.remoteAddress;
-
-    console.log('socket connected from ' + clientIp);
-
+    //Has a little welcome message so that you know you've been connnected
     socket.emit('welcome', {
         text: 'Welcome to Agario'
     });
 
-    socket.on('user', function (name) {
-        console.log(name + ' connected');
-        users.push(name);
-        socket.user = name;
-        console.log('users : ' + users.length);
-        socket.broadcast.emit('otherUserConnect', name);
-        socket.emit('connectUser', {
-            text: name
+    //everytime draw is run data is called to update a users x, y
+    socket.on('data', function (someData) {
+        //It sends this data back to the user's computer to update his array
+        socket.broadcast.emit('update', {
+            x: someData.x,
+            y: someData.y,
+            name: someData.name,
+            id: socket.id
         });
-
     });
 
-    socket.on('size', function (name) {
-        users.push(name);
-        socket.user = name;
+    //sets variable clientIp to their ip, not really important other than for logging
+    var clientIp = socket.request.connection.remoteAddress;
+    //Logs the user's IP
+    console.log('socket connected from ' + clientIp);
+
+    //Called when a new user is made from a browser
+    socket.on('user', function (data) {
+        console.log(data.name + ' connected');
+
+        //Sets placeholder values to show the ellipse offscreen
+        data.x = -500;
+        data.y = -500;
+        data.id = socket.id;
+        //Adds user to the array of users
+        users.push(data);
+
         console.log('users : ' + users.length);
-        socket.broadcast.emit('otherUserConnect', name);
+        //Copies over the existing array of users to the client's computer
+        socket.emit('start', users);
+        //Sends a message out to all other computers saying that a new user is connected
+        socket.broadcast.emit('otherUserConnect', data);
     });
 
+    //When someone quits/closes the window
     socket.on('disconnect', function () {
         if (!socket.user) {
             return;
         }
+        //If a user exists then remove him from the array
         if (users.indexOf(socket.user) > -1) {
             console.log(socket.user + ' disconnected');
             users.splice(users.indexOf(socket.user), 1);
-            socket.broadcast.emit('otherUserDisconnect', socket.user);
+            socket.broadcast.emit('otherUserDisconnect', {
+                name: socket.user
+            });
         }
-        delete currentConnections[socket.id];
     });
 });
